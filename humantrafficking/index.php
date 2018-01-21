15e8eaf6d4fad4d28f5eb6d59178a8eb72764f94
StevenWangler <wangler.steven@outlook.com>
2018-01-20 23:04:47 -0500

    Updated Quick Exit (Temp)

--- humantrafficking/index.php
+++ humantrafficking/index.php
@@ -9,13 +9,13 @@
 <!-- Page Content Index from CMS folder -->
 <?php include "../bulldogcms/frontend/index.php"; ?>
 
-div class="thumbnail" id="quickExit">
-<button type="button" class="btn btn-danger" onclick="quickExit()" data-toggle="tooltip" data-html="true"
-        title="<p>The Quick Exit Button allows you to quickly exit this site<br>and prevent navigation back to it. It works by flooding your browser history and sending you to www.google.com</p>">
-    <span class="glyphicon glyphicon-exclamation-sign"></span> Click To Exit</button>
-<p class="text-center">Get Help Below<br>
-    <span class="glyphicon glyphicon-earphone"></span><a href="tel:+18883737888">1-800-373-7888</a><br>
-    Or text "help" or "info" to <a href="sms:233733">233733</a></p>
+<div class="thumbnail" id="quickExit">
+    <button type="button" class="btn btn-danger" onclick="quickExit()" data-toggle="tooltip" data-html="true"
+            title="<p>The Quick Exit Button allows you to quickly exit this site<br>and prevent navigation back to it. It works by flooding your browser history and sending you to www.google.com</p>">
+        <span class="glyphicon glyphicon-exclamation-sign"></span> Click To Exit</button>
+    <p class="text-center">Get Help Below<br>
+        <span class="glyphicon glyphicon-earphone"></span><a href="tel:+18883737888">1-800-373-7888</a><br>
+        Or text "help" or "info" to <a href="sms:233733">233733</a></p>
 </div>
 
 <!-- Footer -->
