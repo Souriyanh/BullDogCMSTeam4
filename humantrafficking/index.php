0f815e256b22dab2a893ab4c3d87b1aaed9a40fb
StevenWangler <wangler.steven@outlook.com>
2018-01-28 19:57:33 -0500

    This is a change to the quick exit GUI

--- humantrafficking/index.php
+++ humantrafficking/index.php
@@ -9,14 +9,18 @@
 <!-- Page Content Index from CMS folder -->
 <?php include "../bulldogcms/frontend/index.php"; ?>
 
-<!--Here is the quick exit button-->
+<!--Here is our quick exit button-->
 <div class="thumbnail" id="quickExit">
     <button type="button" class="btn btn-danger" onclick="quickExit()" data-toggle="tooltip" data-html="true"
             title="<p>The Quick Exit Button allows you to quickly exit this site<br>and prevent navigation back to it. It works by flooding your browser history and sending you to www.google.com</p>">
         <span class="glyphicon glyphicon-exclamation-sign"></span> Click To Exit</button>
-    <p class="text-center">Get Help Below<br>
-        <span class="glyphicon glyphicon-earphone"></span><a href="tel:+18883737888">1-800-373-7888</a><br>
-        Or text "help" or "info" to <a href="sms:233733">233733</a></p>
+
+    <p class="text-center">Click Below For Help<br></p>
+
+    <button type="button" class="btn btn-info" data-toggle="popover" title="National Human Trafficking Resource Center" data-placement="left" data-html="true"
+            data-content="<h1>Call</h1> 1-800-373-7888 <br> Or <br> Text &quot info or &quot help to 233733">
+
+        <span class="glyphicon glyphicon-info-sign"></span> Get Help</button>
 </div>
 
 <!-- Footer -->
