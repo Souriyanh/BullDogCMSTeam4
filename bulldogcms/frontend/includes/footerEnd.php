13b9184674983ac13dd9aac03fa6b25a6c778b07
jostkyle <jostkyle@gmail.com>
2018-03-28 20:00:11 -0400

    Translate/Foreign Language Support
    Added code & Scripts to the footer to allow for the user to change the language that the site displays in

--- bulldogcms/frontend/includes/footerEnd.php
+++ bulldogcms/frontend/includes/footerEnd.php
@@ -2,6 +2,13 @@
 
 <script src="js/siteSpecificJS.php"></script>
 
+<!-- Google Translate -->
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
 </body>
 
 </html>
